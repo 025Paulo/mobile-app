d4562ab472ce05c26a3dfb188a072f0f43ddf1bc
Paulo Batista <paulo.batista025@outlook.com>
2025-07-22 17:59:57 -0300

    Começo do processo de autenticação

--- lib/auth-context.tsx
+++ lib/auth-context.tsx
@@ -1,18 +1,53 @@
-import { createContext } from "react"
-import { Models } from "react-native-appwrite";
+import { createContext, useContext } from "react";
+import { ID, Models } from "react-native-appwrite";
+import { account } from "./appwrite";
 
 type AuthContextType = {
-    user: Models.User<Models.Preferences> | null;
-    signUp: (email: string, password: string)  => Promise<void>;
-    signIn: (email: string, password: string)  => Promise<void>;
-}
+  // user: Models.User<Models.Preferences> | null;
+  signUp: (email: string, password: string) => Promise<string | null>;
+  signIn: (email: string, password: string) => Promise<string | null>;
+};
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
-export function AuthProvider({children}: { children: React.ReactNode }) {
-  return <></>
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const signUp = async (email: string, password: string) => {
+    try {
+      await account.create(ID.unique(), email, password);
+      await signIn(email, password);
+      return null;
+    } catch (error) {
+      if (error instanceof Error) {
+        return error.message;
+      }
+
+      return "Aconteceu um erro inesperado ao criar a conta. Por favor, tente novamente mais tarde.";
+    }
+  };
+
+  const signIn = async (email: string, password: string) => {
+    try {
+      await account.createEmailPasswordSession(email, password);
+      return null;
+    } catch (error) {
+      if (error instanceof Error) {
+        return error.message;
+      }
+
+      return "Aconteceu um erro inesperado ao fazer login. Por favor, tente novamente mais tarde.";
+    }
+  };
+
+  return (
+    <AuthContext.Provider value={{ signUp, signIn }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
 export function useAuth() {
-
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
 }
